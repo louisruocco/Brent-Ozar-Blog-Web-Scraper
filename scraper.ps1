a2f9896fcb4f4644bc8dfb4ce85432c8e5fc4341
Louis Ruocco <louisruocco1@gmail.com>
2023-12-12 12:31:02 +0000

    able to save links to text file and not write to them if they are already present

--- scraper.ps1
+++ scraper.ps1
@@ -1,4 +1,23 @@
+$path = "C:\users\louis\desktop\test.txt"
+Function DB-Check {
+    if(!(test-path $path)){
+        New-Item $path
+    }
+}
+
 $url = Invoke-WebRequest 'https://www.brentozar.com/archive/category/development/t-sql/'
 $res = $url.ParsedHtml.getElementsByTagName('div') |Where-Object { $_.className -eq 'w-grid-list' }
 $links = $res | ForEach-Object { $_.getElementsByTagName('a') }
-$links.href -replace '#comments', '' -replace 'https://www.brentozar.com/archive/author/brento/', '' -replace 'https://www.brentozar.com/archive/category/videos/', '' -replace 'https://www.brentozar.com/archive/category/development/t-sql/', '' | Select-Object -Unique
+$random = $links.href -replace '#comments', '' -replace 'https://www.brentozar.com/archive/author/brento/', '' -replace 'https://www.brentozar.com/archive/category/videos/', '' -replace 'https://www.brentozar.com/archive/category/development/t-sql/', '' | Select-Object -Unique
+
+function Randomise {
+    $links = $random | Sort-Object {Get-Random}
+    $test = $links[0]
+    $link = Select-String -Path $path -Pattern $test
+    if($link -ne $links[0]){
+        Add-Content -Path $path -Value $test
+    }
+}
+
+DB-Check
+Randomise
