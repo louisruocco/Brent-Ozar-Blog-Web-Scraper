12098bc8960e3ff1a379a35622f977b8d7cc1feb
Louis Ruocco <louisruocco1@gmail.com>
2023-12-12 11:27:34 +0000

    able to grab all blog links from first page

--- scraper.ps1
+++ scraper.ps1
@@ -1,5 +1,4 @@
 $url = Invoke-WebRequest 'https://www.brentozar.com/archive/category/development/t-sql/'
-$res = $url.ParsedHtml.getElementsByTagName('div') |
-  Where-Object { $_.className -eq 'w-grid-list' }
+$res = $url.ParsedHtml.getElementsByTagName('div') |Where-Object { $_.className -eq 'w-grid-list' }
 $links = $res | ForEach-Object { $_.getElementsByTagName('a') }
-$links.href | select -unique
+$links.href -replace '#comments', '' -replace 'https://www.brentozar.com/archive/author/brento/', '' -replace 'https://www.brentozar.com/archive/category/videos/', '' -replace 'https://www.brentozar.com/archive/category/development/t-sql/', '' | Select-Object -Unique
