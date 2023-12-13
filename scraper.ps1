54dd266646acd66f3236761f1a7e4a83d7b1af39
Louis Ruocco <louisruocco1@gmail.com>
2023-12-13 07:24:10 +0000

    added Check-Link function

--- scraper.ps1
+++ scraper.ps1
@@ -1,22 +1,29 @@
-$path = "<insert link here>"
+$url = Invoke-WebRequest 'https://www.brentozar.com/archive/category/development/t-sql/'
+$res = $url.ParsedHtml.getElementsByTagName('div') |Where-Object { $_.className -eq 'w-grid-list' }
+$links = $res | ForEach-Object { $_.getElementsByTagName('a') }
+$random = $links.href -replace '#comments', '' -replace 'https://www.brentozar.com/archive/author/brento/', '' -replace 'https://www.brentozar.com/archive/category/videos/', '' -replace 'https://www.brentozar.com/archive/category/development/t-sql/', '' | Select-Object -Unique
+
+$path = "C:\Users\Louis\Desktop\test.txt"
 Function DB-Check {
     if(!(test-path $path)){
         New-Item $path
     }
 }
 
-$url = Invoke-WebRequest 'https://www.brentozar.com/archive/category/development/t-sql/'
-$res = $url.ParsedHtml.getElementsByTagName('div') |Where-Object { $_.className -eq 'w-grid-list' }
-$links = $res | ForEach-Object { $_.getElementsByTagName('a') }
-$random = $links.href -replace '#comments', '' -replace 'https://www.brentozar.com/archive/author/brento/', '' -replace 'https://www.brentozar.com/archive/category/videos/', '' -replace 'https://www.brentozar.com/archive/category/development/t-sql/', '' | Select-Object -Unique
-
-function Randomise {
-    $links = $random | Sort-Object {Get-Random}
-    $link = $links[0]
+function Check-Link {
+    param (
+        [string]$link, 
+        [string]$links
+    )
     $check = Select-String -Path $path -Pattern $link
     if($check -ne $links[0]){
         Add-Content -Path $path -Value $link
     }
+}
+function Randomise {
+    $links = $random | Sort-Object {Get-Random}
+    $link = $links[0]
+    Check-Link -link $link -links $Links
 }
 
 DB-Check
